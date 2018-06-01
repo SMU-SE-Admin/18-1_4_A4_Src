9cd0cb4018ede94aa8cc92b2c006bdbc2e52a8de
201511017 <uiseo0225@gmail.com>
2018-06-01 16:47:07 +0900

    #1407 할 일 전체보기 기능 구현
    할 일 전체보기 기능 구현

--- src/main/java/se/smu/SeeTodo.java
+++ src/main/java/se/smu/SeeTodo.java
@@ -1,21 +1,35 @@
 package se.smu;
 
 import javax.swing.*;
+
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 
-@SuppressWarnings("serial")
 public class SeeTodo extends JFrame {
 
 	private JPanel contentPane;
 	private JTable table;
 	JScrollPane scroll;
+
+	String colNames2[] = { "항목", "마감기한", "완료여부", "실제마감일", "진행률", "중요도" };
+	DefaultTableModel AllTodoTable = new DefaultTableModel(colNames2, 0);
+
+	Connection conn = null;
+	Statement stmt = null;
+	ResultSet rs = null;
+	String sql = null;
+	PreparedStatement pstmt = null;
 
 	/**
 	 * Launch the application.
@@ -47,18 +61,44 @@ public class SeeTodo extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-		table = new JTable();
-		table.setRowHeight(40);
-		table.setModel(new DefaultTableModel(new Object[][] { { null, null, null, null, null },
-				{ null, null, null, null, null }, { null, null, null, null, null }, { null, null, null, null, null },
-				{ null, null, null, null, null }, { null, null, null, null, null }, { null, null, null, null, null },
-				{ null, null, null, null, null }, { null, null, null, null, null }, { null, null, null, null, null },
-				{ null, null, null, null, null }, { null, null, null, null, null }, },
-				new String[] { "항목", "마감기한", "완료여부", "실제마감일", "중요도" }));
+		table = new JTable(AllTodoTable);
+		table.setAutoCreateRowSorter(true);
 		scroll = new JScrollPane(table);
 		scroll.setBounds(22, 125, 422, 327);
 
 		contentPane.add(scroll);
+		try {
+			Connection conn = null;
+
+			ResultSet rs = null;
+			String sql = null;
+			Class.forName("com.mysql.jdbc.Driver");
+			// Open a connection
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/mydb", "root", "root");
+			stmt = conn.createStatement();
+			/*
+			 * //테이블 생성 전이면 주석 풀기 sql =
+			 * "create table Subject(Subjectname varchar(20), Professor varchar(20), day varchar(20) ,time varchar(20), year varchar(20),semester varchar(20), primary key(Subjectname));"
+			 * ; stmt.executeUpdate(sql);
+			 */
+			System.out.println("테이블을 생성했습니다.");
+
+			sql = "select Todoname,deadline,finish,realdeadline,progressrate,importance  from Todo ;";
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+
+			// 이미 id 존재할 경우
+			while (rs.next()) {
+				// Object data[] =
+				// {rs.getString("Subjectname"),rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5),rs.getString(6)};
+				AllTodoTable.addRow(new Object[] { rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4),
+						rs.getString(5), rs.getString(6) });
+
+			}
+		} catch (Exception ee) {
+			System.out.println("문제있음");
+			ee.printStackTrace();
+		}
 
 		JPanel panel = new JPanel();
 		panel.setBounds(22, 62, 422, 51);
@@ -71,8 +111,8 @@ public class SeeTodo extends JFrame {
 		lblNewLabel.setBounds(14, 12, 139, 27);
 		panel.add(lblNewLabel);
 
-		JButton button_3 = new JButton("-");
-		button_3.setBounds(367, 12, 41, 27);
+		JButton button_3 = new JButton("홈");
+		button_3.setBounds(367, 12, 50, 27);
 		panel.add(button_3);
 		button_3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -83,23 +123,89 @@ public class SeeTodo extends JFrame {
 			}
 		});
 
-		JComboBox<Object> comboBox = new JComboBox<Object>();
-		comboBox.setModel(new DefaultComboBoxModel<Object>(new String[] { "항목", "마감기한", "완료여부", "실제마감일", "중요도" }));
-		comboBox.setBounds(251, 12, 106, 27);
-		panel.add(comboBox);
-
-		final JButton hidebutton = new JButton("숨기기");
+		JButton hidebutton = new JButton("숨기기");
 		hidebutton.setBounds(339, 23, 105, 27);
 		contentPane.add(hidebutton);
 		hidebutton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if (hidebutton.getText().equals("숨기기"))
+				if (hidebutton.getText().equals("숨기기")) {
+					try {
+						Connection conn = null;
+
+						ResultSet rs = null;
+						String sql = null;
+						Class.forName("com.mysql.jdbc.Driver");
+						// Open a connection
+						conn = DriverManager.getConnection("jdbc:mysql://localhost/mydb", "root", "root");
+						stmt = conn.createStatement();
+						/*
+						 * //테이블 생성 전이면 주석 풀기 sql =
+						 * "create table Subject(Subjectname varchar(20), Professor varchar(20), day varchar(20) ,time varchar(20), year varchar(20),semester varchar(20), primary key(Subjectname));"
+						 * ; stmt.executeUpdate(sql);
+						 */
+						System.out.println("테이블을 생성했습니다.");
+
+						sql = "select Todoname,deadline,finish,realdeadline,progressrate,importance  from Todo where finish='X' ;";
+						pstmt = conn.prepareStatement(sql);
+						rs = pstmt.executeQuery();
+						AllTodoTable.setNumRows(0);
+
+						// 이미 id 존재할 경우
+						while (rs.next()) {
+							// Object data[] =
+							// {rs.getString("Subjectname"),rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5),rs.getString(6)};
+							AllTodoTable.addRow(new Object[] { rs.getString(1), rs.getString(2), rs.getString(3),
+									rs.getString(4), rs.getString(5), rs.getString(6) });
+
+						}
+
+					} catch (Exception ee) {
+						System.out.println("문제있음");
+						ee.printStackTrace();
+					}
+
 					hidebutton.setText("보이기");
 
-				else
+				} else if (hidebutton.getText().equals("보이기")) {
+					try {
+						Connection conn = null;
+
+						ResultSet rs = null;
+						String sql = null;
+						Class.forName("com.mysql.jdbc.Driver");
+						// Open a connection
+						conn = DriverManager.getConnection("jdbc:mysql://localhost/mydb", "root", "root");
+						stmt = conn.createStatement();
+						/*
+						 * //테이블 생성 전이면 주석 풀기 sql =
+						 * "create table Subject(Subjectname varchar(20), Professor varchar(20), day varchar(20) ,time varchar(20), year varchar(20),semester varchar(20), primary key(Subjectname));"
+						 * ; stmt.executeUpdate(sql);
+						 */
+						System.out.println("테이블을 생성했습니다.");
+
+						sql = "select Todoname,deadline,finish,realdeadline,progressrate,importance  from Todo  ;";
+						pstmt = conn.prepareStatement(sql);
+						rs = pstmt.executeQuery();
+						AllTodoTable.setNumRows(0);
+
+						// 이미 id 존재할 경우
+						while (rs.next()) {
+							// Object data[] =
+							// {rs.getString("Subjectname"),rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5),rs.getString(6)};
+							AllTodoTable.addRow(new Object[] { rs.getString(1), rs.getString(2), rs.getString(3),
+									rs.getString(4), rs.getString(5), rs.getString(6) });
+
+						}
+
+					} catch (Exception ee) {
+						System.out.println("문제있음");
+						ee.printStackTrace();
+					}
 					hidebutton.setText("숨기기");
+				}
 
 			}
+
 		});
 
 	}
